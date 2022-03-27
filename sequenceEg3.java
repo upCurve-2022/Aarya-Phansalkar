d95f049c51587129d1f3b21b1df8eb56eeb104bd
AaryaPhansalkar <91475266+AaryaPhansalkar@users.noreply.github.com>
2022-03-27 23:02:50 +0530

    Update sequenceEg3.java

--- sequenceEg3.java
+++ sequenceEg3.java
@@ -9,9 +9,9 @@ public class sequenceEg3
         System.out.println("How many terms wanted in sequence : ");
         int  seqNo = sc.nextInt();
         int [] seqArray = new int[seqNo];
-        for(int i=0;i<seqNo;i++)
+        for(int i=1;i<seqNo;i++)
         {
-           seqArray[i] = (int) Math.pow(i+1,i+1);
+           seqArray[i-1] = (int) Math.pow(i,i);
         }
         for(int i=0;i<seqNo;i++)
         {
