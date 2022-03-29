45990ff8890db8aae460462c44c18c7b56e7e301
AaryaPhansalkar <91475266+AaryaPhansalkar@users.noreply.github.com>
2022-03-29 22:26:12 +0530

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -6,17 +6,16 @@ public class isPalindrome
     public static void main(String[] args)
     {
             Scanner sc=new Scanner(System.in);
-            System.out.println("How many terms wanted in sequence : ");
+            System.out.println("Enter string : ");
             String testString = sc.next();
             String toCheck ="";
             for(int i=testString.length()-1;i>=0;i--)
             {
                 toCheck += testString.charAt(i);
             }
-            System.out.println("Original string is :"+testString+" and reverse string is :"+toCheck);
+            System.out.println("Original string is :"+testString+"\nReverse string is :"+toCheck);
             if(toCheck.equals(testString))
             {
-
                 System.out.println("String is palindrome");
             }
             else
