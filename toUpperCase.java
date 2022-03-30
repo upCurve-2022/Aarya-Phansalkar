58a327f49e80b759bee390dffce44e77fbef4fca
AaryaPhansalkar <91475266+AaryaPhansalkar@users.noreply.github.com>
2022-03-30 18:08:17 +0530

    Add files via upload

---
+++ toUpperCase.java
@@ -0,0 +1,27 @@
+package com.javaAssignment;
+
+import java.util.Scanner;
+
+public class toUpperCase
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter string : ");
+        String testString = sc.next();
+        String upperCaseString = "";
+        for(int i=0;i<testString.length();i++)
+        {
+            if(testString.charAt(i)>=97 && testString.charAt(i)<=122)
+            {
+                upperCaseString+=(char)(testString.charAt(i)-32);
+            }
+            else
+            {
+                upperCaseString+=testString.charAt(i);
+            }
+
+        }
+        System.out.println(upperCaseString);
+    }
+}
